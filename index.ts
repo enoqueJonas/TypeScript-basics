c64c45ae4fc2a610cad65e009fa64ed7d46a1ac7
enoqueJonas <enoque.macanda@gmail.com>
2023-08-29 11:16:55 +0200

    Variable types

---
+++ index.ts
@@ -0,0 +1,21 @@
+// Types
+  // Primitive -> number, boolean, void, undefined, string, symbol, null
+  const apples: number = 5; // Type annotation (We tell javaScript what is type of value the variable will hold)
+  const myName: string = "Enoque";
+  const isEnough: boolean = false;
+
+  // Objects -> functions, arrays, objects, classes
+  const date: Date = new Date();
+  const colors: string[] = ['red', 'green', 'blue']
+
+  class Car {}
+  const car: Car = new Car();
+
+  const point: { x: number; y: number } = {
+    x: 10,
+    y: 20,
+  }
+
+  const logNumber: (i: number) => void = (i) => {
+    console.log(i)
+  }
